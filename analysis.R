14f61c4b3591ae81c67c0a4beb64b3d6b8831b11
jvpoulos <jvpoulos@gmail.com>
2016-07-13 22:47:04 -0400

    analysis.R:

--- analysis.R
+++ analysis.R
@@ -178,14 +178,14 @@ slaves.CI <- BootDiff(y=resp.dat[!is.na(resp.dat$no.slaves.1820),]$no.slaves.182
 
 # Create data for plot
 plot.data <- data.frame(x = c("ITT","TOT"),
-                        y = c(candidate.CI[1],candidate.CI[2],
+                        y = c(bank.CI[1],bank.CI[2],
+                              candidate.CI[1],candidate.CI[2],
                               oh.CI[1],oh.CI[2],
-                              bank.CI[1],bank.CI[2],
                               slavery.CI[1],slavery.CI[2],
-                              term.CI[1],term.CI[2],
-                              slaves.CI[1],slaves.CI[2]),
-                        y.lo = c(candidate.CI[3],candidate.CI[4],oh.CI[3],oh.CI[4],bank.CI[3],bank.CI[4],slavery.CI[3],slavery.CI[4],term.CI[3],term.CI[4],slaves.CI[3],slaves.CI[4]),
-                        y.hi = c(candidate.CI[5],candidate.CI[6],oh.CI[5],oh.CI[6],bank.CI[5],bank.CI[6],slavery.CI[5],slavery.CI[6],term.CI[5],term.CI[6],slaves.CI[5],slaves.CI[6]))
+                              slaves.CI[1],slaves.CI[2],
+                              term.CI[1],term.CI[2]),
+                        y.lo = c(bank.CI[3],bank.CI[4],candidate.CI[3],candidate.CI[4],oh.CI[3],oh.CI[4],slavery.CI[3],slavery.CI[4],slaves.CI[3],slaves.CI[4],term.CI[3],term.CI[4]),
+                        y.hi = c(bank.CI[5],bank.CI[6],candidate.CI[5],candidate.CI[6],oh.CI[5],oh.CI[6],slavery.CI[5],slavery.CI[6],slaves.CI[5],slaves.CI[6],term.CI[5],term.CI[6]))
 plot.data <- transform(plot.data, y.lo = y.lo, y.hi=y.hi)
 plot.data$Outcome <- c(rep(paste("Banking legislation, N =", 
                                  format(nrow(sub.prior[!is.na(sub.prior$bank.index),]),big.mark=",",scientific=FALSE,trim=TRUE)),2),
@@ -193,15 +193,17 @@ plot.data$Outcome <- c(rep(paste("Banking legislation, N =",
                                  format(nrow(sub.candidate),big.mark=",",scientific=FALSE,trim=TRUE)),2),
                        rep(paste("Officeholding, N =",
                                  format(nrow(sub.oh),big.mark=",",scientific=FALSE,trim=TRUE)),2),
-                       rep(paste("# Slaves, N =", 
-                                 format(nrow(resp.dat[!is.na(resp.dat$no.slaves.1820),]),big.mark=",",scientific=FALSE,trim=TRUE)),2),
                        rep(paste("Slavery legislation, N =", 
                                  format(nrow(sub.prior[!is.na(sub.prior$slave.index),]),big.mark=",",scientific=FALSE,trim=TRUE)),2),
+                       rep(paste("# Slaves, N =", 
+                                 format(nrow(resp.dat[!is.na(resp.dat$no.slaves.1820),]),big.mark=",",scientific=FALSE,trim=TRUE)),2),
                        rep(paste("# Terms, N=", 
                                      format(nrow(sub.prior),big.mark=",",scientific=FALSE,trim=TRUE)),2))
 
+
 # Plot forest plot
 plot.data$x <- factor(plot.data$x, levels=rev(plot.data$x)) # reverse order
+plot.data$Outcome <- factor(plot.data$Outcome, levels=plot.data$Outcome) # reverse order
 summary.plot <- ForestPlot(plot.data,xlab="Treatment effect",ylab="Analysis")
 
 ggsave(paste0(data.directory,"summary-plot.pdf"), summary.plot, width=8.5, height=11)
